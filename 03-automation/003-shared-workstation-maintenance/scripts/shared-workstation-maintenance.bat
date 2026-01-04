47f32e2ee6c2ab69b16640456115920d66022321
Commoner <79024012+DamingHuang@users.noreply.github.com>
2026-01-04 01:44:12 -0500

    Update shared-workstation-maintenance.bat

--- 03-automation/003-shared-workstation-maintenance/scripts/shared-workstation-maintenance.bat
+++ 03-automation/003-shared-workstation-maintenance/scripts/shared-workstation-maintenance.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Script started at %date% %time% >> C:\Users\Public\Scripts\Public\log.txt
-powershell.exe -ExecutionPolicy Bypass -File "C:\Users\Public\Scripts\Public\Public.ps1" >> C:\Users\Public\Scripts\Public\log.txt 2>&1
+powershell.exe -ExecutionPolicy Bypass -File "C:\Users\Public\Scripts\Public\shared-workstation-maintenance.ps1" >> C:\Users\Public\Scripts\Public\log.txt 2>&1
 echo Script finished at  %date% %time% >> C:\Users\Public\Scripts\Public\log.txt
