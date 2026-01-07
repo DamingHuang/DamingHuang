2abbbae205e356ed889357ec29c040b7fc25e969
Commoner <79024012+DamingHuang@users.noreply.github.com>
2026-01-07 13:47:56 -0500

    Add number checker for positive, zero, and negative

---
+++ 04-python/python-learning-path/control_flow/number_checker.py
@@ -0,0 +1,9 @@
+number = input('Please input a number')
+float(number) #change data type
+
+if number > 0:
+   print('Number is positive.')
+elif number == 0:
+   print('Number is zero.')
+else:
+   print('Number is negative.')
