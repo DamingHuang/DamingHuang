0a32ae664ca773feaa973d1493cf2bb2ddf7ff0d
Commoner <79024012+DamingHuang@users.noreply.github.com>
2026-01-12 16:38:57 -0500

    Delete 03-automation /004-vm-resource-remediation directory

--- 03-automation /004-vm-resource-remediation/script/stop_service.ps1
+++
@@ -1,8 +0,0 @@
-$serviceNames = @("wuauserv", "WaaSMedicSvc", "usosvc")
-$services = Get-Service -Name $serviceNames
-
-foreach ($s in $services) {
-    if ($s.Status -eq 'Running') {
-        net stop $s.Name
-    }
-}
