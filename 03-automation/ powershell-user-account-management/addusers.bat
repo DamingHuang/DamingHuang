33a5339344cb671e383f03600013c6c09ddecb04
Commoner <79024012+DamingHuang@users.noreply.github.com>
2026-01-23 16:11:34 -0500

    Update and rename adusers.bat to addusers.bat
    corrected the file path of the ps1 file

--- 03-automation/ powershell-user-account-management/addusers.bat
+++ 03-automation/ powershell-user-account-management/addusers.bat
@@ -7,5 +7,5 @@ REM 1. Replace "your-path" with the folder where the script and logs will be sto
 REM 2. Replace "adusers.ps1" with the name of your PowerShell script.
 
 echo Script started at %date% %time% >> "your-path\log.txt"
-powershell.exe -ExecutionPolicy Bypass -File "your-path\log.txt\adusers.ps1" >> "your-path\errlog.txt"  2>&1
+powershell.exe -ExecutionPolicy Bypass -File "your-path\log.txt\addusers.ps1" >> "your-path\errlog.txt"  2>&1
 echo Script finished at  %date% %time% >> "your-path\log.txt"
