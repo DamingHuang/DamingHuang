5583595ed89c43cdb61131ab1db58f4a29458388
Commoner <79024012+DamingHuang@users.noreply.github.com>
2026-01-23 16:15:08 -0500

    Update remove-user.bat to call rmusers.ps1

---
+++ 03-automation/ powershell-user-account-management/remove-user.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM -------------------------------
+REM Batch file to run AD User PowerShell script
+REM -------------------------------
+REM Instructions:
+REM 1. Replace "your-path" with the folder where the script and logs will be stored.
+REM 2. Replace "remove-user.ps1" with the name of your PowerShell script.
+
+echo Script started at %date% %time% >> "your-path\log.txt"
+powershell.exe -ExecutionPolicy Bypass -File "your-path\rmusers.ps1" >> "your-path\errlog.txt" 2>&1
+
+echo Script finished at  %date% %time% >> "your-path\log.txt"
