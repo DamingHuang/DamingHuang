cc38f4ee13edd73327b5f35ee5bdb648471d9bbd
Commoner <79024012+DamingHuang@users.noreply.github.com>
2026-07-27 21:55:52 -0400

    Add start.bat to execute net.ps1 script

---
+++ technical-portfolio/03-automation/010-auto-scan-v1/start.bat
@@ -0,0 +1,6 @@
+@echo off
+powershell -ExecutionPolicy Bypass -File "%~dp0net.ps1"
+
+echo  ---------------------------------------------------------------------------------
+echo script finished.
+powershell -NoExit
